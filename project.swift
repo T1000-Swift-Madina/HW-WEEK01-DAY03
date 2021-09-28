46b9c6303ff1d77bb25243a3d70e9f86b9564bc0
Noura.alahmadi.med.s@tuwaiq.edu.sa <Teeko1122>
2021-09-28 22:20:00 +0300

    About me

---
+++ project.swift
@@ -0,0 +1,7 @@
+// try the git
+var name = "nora"
+var age = 23
+var dgree = 4.13
+print("my name is \(name) iam \(age) years old i graduated from taibah universty with \(dgree)")
+
+print("i like git , make my life easy")
