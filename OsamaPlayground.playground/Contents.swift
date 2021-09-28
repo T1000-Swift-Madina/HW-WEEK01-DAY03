a54f7f982b1ffbbc5a1cdb7ab3436ad3639e78ce
Osama folta <Osama.Fallatah.med.s@tuwaiq.edu.sa>
2021-09-29 02:00:04 +0300

    modify ed

---
+++ OsamaPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+// int
+var speedlmt = 80
+let name = "osama"
+var nDubl = 1.5
+var nm:Int
+print(nDubl)
+print("my name is \(name)")
+speedlmt += 10
+print("new road speed ", speedlmt)
+
